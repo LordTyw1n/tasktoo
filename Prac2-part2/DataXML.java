f504e9ec1923568953d361feac1677657468f0d4
LordTyw1n <100356990+LordTyw1n@users.noreply.github.com>
2024-05-22 18:54:06 +0200

    First DataXML.java

---
+++ Prac2-part2/DataXML.java
@@ -0,0 +1,40 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class ReadXMLFile {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("data.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("record");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println("Postal Zip : " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
+                    System.out.println("Region : " + eElement.getElementsByTagName("region").item(0).getTextContent());
+                    System.out.println("Country : " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                    System.out.println("Address : " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                    System.out.println("List : " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
