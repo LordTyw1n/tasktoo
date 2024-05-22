1c47965bf3b7720c012768bb84097e308b843bc3
LordTyw1n <100356990+LordTyw1n@users.noreply.github.com>
2024-05-22 18:56:04 +0200

    Second  DataXML.java

--- Prac2-part2/DataXML.java
+++ Prac2-part2/DataXML.java
@@ -5,9 +5,23 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ReadXMLFile {
     public static void main(String[] args) {
+        // Check if user provided fields as command-line arguments
+        if (args.length == 0) {
+            System.out.println("Please specify the fields to be printed (e.g., name postalZip region).");
+            return;
+        }
+
+        // Create a set of selected fields for quick lookup
+        Set<String> selectedFields = new HashSet<>();
+        for (String field : args) {
+            selectedFields.add(field.toLowerCase());
+        }
+
         try {
             File inputFile = new File("data.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -15,22 +29,34 @@ public class ReadXMLFile {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
 
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
             NodeList nList = doc.getElementsByTagName("record");
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                System.out.println("\nCurrent Element: " + nNode.getNodeName());
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
 
-                    System.out.println("Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
-                    System.out.println("Postal Zip : " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
-                    System.out.println("Region : " + eElement.getElementsByTagName("region").item(0).getTextContent());
-                    System.out.println("Country : " + eElement.getElementsByTagName("country").item(0).getTextContent());
-                    System.out.println("Address : " + eElement.getElementsByTagName("address").item(0).getTextContent());
-                    System.out.println("List : " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                    if (selectedFields.contains("name")) {
+                        System.out.println("Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    }
+                    if (selectedFields.contains("postalzip")) {
+                        System.out.println("Postal Zip: " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
+                    }
+                    if (selectedFields.contains("region")) {
+                        System.out.println("Region: " + eElement.getElementsByTagName("region").item(0).getTextContent());
+                    }
+                    if (selectedFields.contains("country")) {
+                        System.out.println("Country: " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                    }
+                    if (selectedFields.contains("address")) {
+                        System.out.println("Address: " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                    }
+                    if (selectedFields.contains("list")) {
+                        System.out.println("List: " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                    }
                 }
             }
         } catch (Exception e) {
