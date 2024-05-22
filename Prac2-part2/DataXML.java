8370cfac6bf92acd2de6e06c0ecac8aa3c957a3b
LordTyw1n <100356990+LordTyw1n@users.noreply.github.com>
2024-05-22 19:03:25 +0200

    Third DataXML json Edit added

--- Prac2-part2/DataXML.java
+++ Prac2-part2/DataXML.java
@@ -4,6 +4,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.json.JSONObject;
+import org.json.JSONArray;
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
@@ -29,36 +31,41 @@ public class ReadXMLFile {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
 
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
             NodeList nList = doc.getElementsByTagName("record");
+            JSONArray recordsArray = new JSONArray();
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-                System.out.println("\nCurrent Element: " + nNode.getNodeName());
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
+                    JSONObject recordObject = new JSONObject();
 
                     if (selectedFields.contains("name")) {
-                        System.out.println("Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                        recordObject.put("name", eElement.getElementsByTagName("name").item(0).getTextContent());
                     }
                     if (selectedFields.contains("postalzip")) {
-                        System.out.println("Postal Zip: " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
+                        recordObject.put("postalZip", eElement.getElementsByTagName("postalZip").item(0).getTextContent());
                     }
                     if (selectedFields.contains("region")) {
-                        System.out.println("Region: " + eElement.getElementsByTagName("region").item(0).getTextContent());
+                        recordObject.put("region", eElement.getElementsByTagName("region").item(0).getTextContent());
                     }
                     if (selectedFields.contains("country")) {
-                        System.out.println("Country: " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                        recordObject.put("country", eElement.getElementsByTagName("country").item(0).getTextContent());
                     }
                     if (selectedFields.contains("address")) {
-                        System.out.println("Address: " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                        recordObject.put("address", eElement.getElementsByTagName("address").item(0).getTextContent());
                     }
                     if (selectedFields.contains("list")) {
-                        System.out.println("List: " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                        recordObject.put("list", eElement.getElementsByTagName("list").item(0).getTextContent());
                     }
+
+                    recordsArray.put(recordObject);
                 }
             }
+
+            // Output the JSON array
+            System.out.println(recordsArray.toString(4)); // Pretty print with an indent of 4
         } catch (Exception e) {
             e.printStackTrace();
         }
